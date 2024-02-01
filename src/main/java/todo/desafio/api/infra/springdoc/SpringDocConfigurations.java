c2656583198fa0db7859b05be2b302c39168f1ff
rstevan <riirezende@gmail.com>
2024-02-01 18:42:03 -0300

    incluindo documentacao na api

---
+++ src/main/java/todo/desafio/api/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,36 @@
+package todo.desafio.api.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Todo Tarefas - API")
+                        .description("API Rest da aplicação Todo Lista de Tarefas, contendo as funcionalidades de CRUD de novas tarefas")
+                        .contact(new Contact()
+                                .name("Time Backend")
+                                .email("richardrezende_ti@outlook.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("")));
+    }
+
+
+}
